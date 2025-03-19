59305efed8e8c0b7abffcddb3c0307c18371b0e4
ShawnZL <1140068696@qq.com>
2025-03-19 11:15:52 +0800

    😰

---
+++ Actor-Critic/main.py
@@ -0,0 +1,92 @@
+import gym
+import torch
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import util
+
+
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
+
+class ValueNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim):
+        super(ValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+class ActorCritic:
+    def __init__(self, state_dim, action_dim, hidden_dim, actor_lr, critic_lr, device, gamma=0.99):
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.critic = ValueNet(state_dim, hidden_dim).to(device)
+        # 策略优化器
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
+        self.gamma = gamma
+        self.device = device
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float32).to(self.device)
+        probs = self.actor(state)
+        dist = torch.distributions.Categorical(probs)
+        action = dist.sample()
+        return action.item()
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(device=self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(device=self.device)
+        rewards = torch.tensor(transition_dict['rewards']).view(-1, 1).to(device=self.device)
+        next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(device=self.device)
+        dones = torch.tensor(transition_dict['dones']).view(-1, 1).to(device=self.device)
+
+        # 需要学习的
+        td_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
+
+        # 优势函数
+        td_delta = td_target - self.critic(states)
+        log_probs = torch.log(self.actor(states).gather(1, actions))
+        actor_loss = torch.mean(-log_probs * td_delta.detach())
+
+        # 均方误差损失函数
+        critic_loss = torch.mean(F.mse_loss(self.critic(states), td_target.detach()))
+
+        self.actor_optimizer.zero_grad()
+        self.critic_optimizer.zero_grad()
+
+        actor_loss.backward()
+        critic_loss.backward()
+
+        self.actor_optimizer.step()
+        self.critic_optimizer.step()
+
+actor_lr = 1e-3
+critic_lr = 1e-2
+num_episodes = 1000
+hidden_dim = 128
+gamma = 0.98
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+    "cpu")
+
+env_name = 'CartPole-v0'
+env = gym.make(env_name)
+env.seed(0)
+torch.manual_seed(0)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = ActorCritic(state_dim, hidden_dim, action_dim, actor_lr, critic_lr,
+                    gamma, device)
+
+return_list = util.train_on_policy_agent(env, agent, num_episodes)
+
+
