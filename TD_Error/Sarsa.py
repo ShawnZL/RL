0f04f34c6bef4e0c677de1b1eb92ef1445cd336d
ShawnZL <1140068696@qq.com>
2025-04-10 23:16:31 +0800

    td-error

--- TD_Error/Sarsa.py
+++ TD_Error/Sarsa.py
@@ -57,47 +57,212 @@ class Sarsa():
         td_error = r + self.gamma * self.Q_table[s1, a1] - self.Q_table[s0, a0]
         self.Q_table[s0, a0] += self.alpha * td_error
 
-ncol = 12
-nrow = 4
-env = CliffWalkingEnv(ncol, nrow)
-np.random.seed(0)
-epsilon = 0.1
-alpha = 0.1
-gamma = 0.9
-agent = Sarsa(ncol, nrow, epsilon, alpha, gamma)
-num_episodes = 500  # 智能体在环境中运行的序列的数量
+class nstep_Sarsa:
+    def __init__(self, n, ncol, nrow, epsilon, alpha, gamma, n_action=4):
+        self.Q_table = np.zeros([nrow * ncol, n_action])
+        self.n_action = n_action
+        self.alpha = alpha # 学习率
+        self.gamma = gamma # 下个状态的衰减率
+        self.epsilon = epsilon
+        self.n = n
+        self.state_list = [] # 保存之前的状态
+        self.action_list = []
+        self.reward_list = []
 
-return_list = []  # 记录每一条序列的回报
-for i in range(10):  # 显示10个进度条
-    # tqdm的进度条功能
-    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-        for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
-            episode_return = 0
-            state = env.reset()
-            action = agent.take_action(state)
-            done = False
-            while not done:
-                next_state, reward, done = env.step(action)
-                next_action = agent.take_action(next_state)
-                episode_return += reward  # 这里回报的计算不进行折扣因子衰减
-                agent.update(state, action, reward, next_state, next_action)
-                state = next_state
-                action = next_action
-            return_list.append(episode_return)
-            if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
-                pbar.set_postfix({
-                    'episode':
-                    '%d' % (num_episodes / 10 * i + i_episode + 1),
-                    'return':
-                    '%.3f' % np.mean(return_list[-10:])
-                })
-            pbar.update(1)
+    def take_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            action = np.random.choice(self.n_action)
+        else:
+            action = np.argmax(self.Q_table[state])
+        return action
 
-episodes_list = list(range(len(return_list)))
-plt.plot(episodes_list, return_list)
-plt.xlabel('Episodes')
-plt.ylabel('Returns')
-plt.title('Sarsa on {}'.format('Cliff Walking'))
-plt.show()
+    def best_action(self, state):
+        Q_max = np.max(self.Q_table[state])
+        a = [0 for _ in range(self.n_action)]
+        for i in range(self.n_action):
+            if self.Q_table[state, i] == Q_max:
+                a[i] = 1
+        return a
+
+    def update(self, s0, a0, r, s1, a1, done):
+        self.state_list.append(s0)
+        self.action_list.append(a0)
+        self.reward_list.append(r)
+        if len(self.state_list) == self.n: # 可以进行n step learning
+            G = self.Q_table[s1, a1] # get Q(s_{t+n}, a_{t+n})
+            for i in reversed(range(self.n)):
+                G = self.gamma * G + self.reward_list[i]
+                # if reach the end
+                if done and i > 0:
+                    s = self.state_list[i]
+                    a = self.action_list[i]
+                    self.Q_table[s, a] += self.alpha * (G - self.Q_table[s, a])
+            s = self.state_list.pop(0)
+            a = self.action_list.pop(0)
+            self.reward_list.pop(0)
+            # n step sarsa
+            self.Q_table[s, a] += self.alpha * (G - self.Q_table[s, a])
+        if done:
+            self.state_list = []
+            self.action_list = []
+            self.reward_list = []
+
+class QLearning:
+    def __init__(self, nrow, ncol, epsilon, alpha, gamma, n_action=4):
+        self.Q_table = np.zeros([nrow * ncol, n_action])
+        self.n_action = n_action
+        self.alpha = alpha # 学习率
+        self.gamma = gamma # 衰减率
+        self.epsilon = epsilon
+
+    def take_action(self, state): # 选取下一步动作
+        if np.random.rand() <= self.epsilon:
+            action = np.random.choice(self.n_action)
+        else:
+            action = np.argmax(self.Q_table[state])
+        return action
+
+    def best_action(self, state):
+        Q_max = np.max(self.Q_table[state])
+        a = [0 for _ in range(self.n_action)]
+        for i in range(self.n_action):
+            if self.Q_table[state, i] == Q_max:
+                a[i] = 1
+        return a
+
+    def update(self, s0, a0, r, s1):
+        # 注意这里，之前是td_error = r + self.gamma * self.Q_table[s1, a1] - self.Q_table[s0, a0]
+        # 也就是a1 是采样得到，但是Q_learning 不需要采样，只需要知道经过s1的最大值即可
+        td_error = r + self.gamma * self.Q_table[s1].max() - self.Q_table[s0, a0]
+        self.Q_table[s0, a0] += self.alpha * td_error
 
 
+if __name__ == "__main__":
+    ncol = 12
+    nrow = 4
+    env = CliffWalkingEnv(ncol, nrow)
+    """
+    # sarsa
+    np.random.seed(0)
+    epsilon = 0.1
+    alpha = 0.1
+    gamma = 0.9
+    agent = Sarsa(ncol, nrow, epsilon, alpha, gamma)
+    num_episodes = 500  # 智能体在环境中运行的序列的数量
+    
+    return_list = []  # 记录每一条序列的回报
+    for i in range(10):  # 显示10个进度条
+        # tqdm的进度条功能
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+                episode_return = 0
+                state = env.reset()
+                action = agent.take_action(state)
+                done = False
+                while not done:
+                    next_state, reward, done = env.step(action)
+                    next_action = agent.take_action(next_state)
+                    episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                    agent.update(state, action, reward, next_state, next_action)
+                    state = next_state
+                    action = next_action
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                    pbar.set_postfix({
+                        'episode':
+                        '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                        '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+    
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('Sarsa on {}'.format('Cliff Walking'))
+    plt.show()
+    """
+
+    """
+    # n step sarsa
+    np.random.seed(0)
+    n_step = 5  # 5步Sarsa算法
+    alpha = 0.1
+    epsilon = 0.1
+    gamma = 0.9
+    agent = nstep_Sarsa(n_step, ncol, nrow, epsilon, alpha, gamma)
+    num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+    return_list = []  # 记录每一条序列的回报
+    for i in range(10):  # 显示10个进度条
+        # tqdm的进度条功能
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+                episode_return = 0
+                state = env.reset()
+                action = agent.take_action(state)
+                done = False
+                while not done:
+                    next_state, reward, done = env.step(action)
+                    next_action = agent.take_action(next_state)
+                    episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                    agent.update(state, action, reward, next_state, next_action,
+                                 done)
+                    state = next_state
+                    action = next_action
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('5-step Sarsa on {}'.format('Cliff Walking'))
+    plt.show()
+    """
+
+    np.random.seed(0)
+    epsilon = 0.1
+    alpha = 0.1
+    gamma = 0.9
+    agent = QLearning(ncol, nrow, epsilon, alpha, gamma)
+    num_episodes = 500  # 智能体在环境中运行的序列的数量
+
+    return_list = []  # 记录每一条序列的回报
+    for i in range(10):  # 显示10个进度条
+        # tqdm的进度条功能
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):  # 每个进度条的序列数
+                episode_return = 0
+                state = env.reset()
+                done = False
+                while not done:
+                    action = agent.take_action(state)
+                    next_state, reward, done = env.step(action)
+                    episode_return += reward  # 这里回报的计算不进行折扣因子衰减
+                    agent.update(state, action, reward, next_state)
+                    state = next_state
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:  # 每10条序列打印一下这10条序列的平均回报
+                    pbar.set_postfix({
+                        'episode':
+                            '%d' % (num_episodes / 10 * i + i_episode + 1),
+                        'return':
+                            '%.3f' % np.mean(return_list[-10:])
+                    })
+                pbar.update(1)
+
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('Q-learning on {}'.format('Cliff Walking'))
+    plt.show()
