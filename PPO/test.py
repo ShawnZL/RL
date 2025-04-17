f4f893310a1d4fd30a5ab96040fc92989a14a236
ShawnZL <1140068696@qq.com>
2025-04-17 19:47:15 +0800

    PPO

--- PPO/test.py
+++ PPO/test.py
@@ -81,3 +81,26 @@ class PPO:
             critic_loss.backward()
             self.actor_optimizer.step()
             self.critic_optimizer.step()
+
+if __name__ == '__main__':
+    actor_lr = 1e-3
+    critic_lr = 1e-2
+    num_episodes = 500
+    hidden_dim = 128
+    gamma = 0.98
+    lmbda = 0.95
+    epochs = 10
+    eps = 0.2
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
+        "cpu")
+
+    env_name = 'CartPole-v0'
+    env = gym.make(env_name)
+    env.seed(0)
+    torch.manual_seed(0)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    agent = PPO(state_dim, hidden_dim, action_dim, actor_lr, critic_lr, lmbda,
+                epochs, eps, gamma, device)
+
+    return_list = rl_utils.train_on_policy_agent(env, agent, num_episodes)
